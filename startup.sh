8d83d87b3c62da64d398763cae63b1e977d25d5f
allistairhakim <24ah0222@wwprsd.org>
2024-07-16 15:32:10 -0400

    Fixed startup.sh

--- startup.sh
+++ startup.sh
@@ -18,8 +18,7 @@ done
 # Initialize the database
 mysql -u admin -p"{DB_PASS}" -h "{DB_HOST}" -P 3306 -D "{DB_NAME}" < /var/www/html/database.sql
 
-# Update the config file
-sed -i "s|\$dbhost = '';|\$dbhost = '{DB_HOST}';|; s|\$dbuser = '';|\$dbuser = 'admin';|; s|\$dbpass = '';|\$dbpass = '{DB_PASS}';|; s|\$dbname = '';|\$dbname = '{DB_NAME}';|" /var/www/html/config.php
+sed -i 's|\$dbhost = '"'"''"'"';|\$dbhost = '"'"'{DB_HOST}'"'"';|; s|\$dbuser = '"'"''"'"';|\$dbuser = '"'"'admin'"'"';|; s|\$dbpass = '"'"''"'"';|\$dbpass = '"'"'{DB_PASS}'"'"';|; s|\$dbname = '"'"''"'"';|\$dbname = '"'"'{DB_NAME}'"'"';|' /var/www/html/config.php
 
 # Start Apache in the foreground
 apache2ctl -D FOREGROUND
