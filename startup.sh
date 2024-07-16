39a7f1c7bb0254b12b1b6ecf495a9372528d4747
allistairhakim <24ah0222@wwprsd.org>
2024-07-16 17:00:19 -0400

    Fixed startup.sh

--- startup.sh
+++ startup.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+# Move sample config to actual config
 mv config.sample.php config.php
+
+# Update Apache configuration to allow .htaccess overrides
 sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
 
 # Set ServerName in Apache configuration
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
-# Connect and setup database
-echo "{DB_PASS} {DB_HOST} {DB_NAME}"
+# Connect and setup database credentials
+DB_PASSWORD="{DB_PASS}"
+DB_HOST="{DB_HOST}"
+DB_NAME="{DB_NAME}"
+DB_USER="admin"
 
 # Wait for MySQL to be ready
-until mysql -u admin -p"{DB_PASS}" -h ${DB_HOST}" -P 3306 -e "SELECT 1"; do
+until mysql -u $DB_USER -p"$DB_PASSWORD" -h "$DB_HOST" -P 3306 -e "SELECT 1"; do
     echo "Waiting for MySQL to be ready..."
     sleep 2
 done
 
 # Initialize the database
-mysql -u admin -p"{DB_PASS}" -h "{DB_HOST}" -P 3306 -D "{DB_NAME}" < /var/www/html/database.sql
+mysql -u $DB_USER -p"$DB_PASSWORD" -h "$DB_HOST" -P 3306 -D "$DB_NAME" < /var/www/html/database.sql
 
-sed -i 's|\$dbhost = '"'"''"'"';|\$dbhost = '"'"'{DB_HOST}'"'"';|; s|\$dbuser = '"'"''"'"';|\$dbuser = '"'"'admin'"'"';|; s|\$dbpass = '"'"''"'"';|\$dbpass = '"'"'{DB_PASS}'"'"';|; s|\$dbname = '"'"''"'"';|\$dbname = '"'"'{DB_NAME}'"'"';|' /var/www/html/config.php
+# Update the config.php file with database credentials
+sed -i "s|\$dbhost = ''|\$dbhost = '$DB_HOST'|; s|\$dbuser = ''|\$dbuser = '$DB_USER'|; s|\$dbpass = ''|\$dbpass = '$DB_PASSWORD'|; s|\$dbname = ''|\$dbname = '$DB_NAME'|" /var/www/html/config.php
 
 # Start Apache in the foreground
 apache2ctl -D FOREGROUND
